8f1be1cc82c7d6009b7c697da5c6dc26a2c2d24c
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-15 00:50:06 +0500

    create ask question page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -103,8 +103,8 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {result.questions.length > 0 ? (
-          result.questions.map((question) => (
+        {result?.questions.length > 0 ? (
+          result?.questions.map((question) => (
             <QuestionCard
               key={question?.id}
               id={question?.id}
