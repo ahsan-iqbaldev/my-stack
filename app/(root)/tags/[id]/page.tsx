30790a1045b68844ded4fcbfe2d8f32e0f947db6
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-14 09:39:31 +0500

    create tag detail page and Skeleton  loading

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,84 @@
+import QuestionCard from "@/components/shared/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import Pagination from "@/components/shared/Pagination";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { URLProps } from "@/types";
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = {
+    questions: [
+      {
+        id: "9183809",
+        title: "Ahsan Iqbal",
+        tags: [
+          {
+            id: "83984",
+            name: "Ahsaniqbal",
+          },
+        ],
+        author: {
+          id: "49820948290",
+          name: "Ahsan Author",
+          picture: "/assets/images/logo.png",
+          clerkId: "8094892374824",
+        },
+        upvotes: [],
+        views: 4,
+        answers: [],
+        createdAt: new Date(),
+      },
+    ],
+    isNext: false,
+    tagTitle: "Tag Title",
+  };
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route={`/tags/${params.id}`}
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question.id}
+              id={question.id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no tag question saved to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Page;
