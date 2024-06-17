542bee88834b5a202b7fbbdcc27ba1e92905946e
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-18 01:54:37 +0500

    Update validations.tsx

--- lib/validations.tsx
+++ lib/validations.tsx
@@ -1,14 +1,14 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const QuestionsSchema = z.object({
   title: z.string().min(5).max(130),
   explanation: z.string().min(100),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
-})
+});
 
 export const AnswerSchema = z.object({
-  answer: z.string().min(100)
-})
+  answer: z.string().min(100),
+});
 
 export const ProfileSchema = z.object({
   name: z.string().min(5).max(50),
@@ -16,4 +16,5 @@ export const ProfileSchema = z.object({
   bio: z.string().min(10).max(150),
   portfolioWebsite: z.string().url(),
   location: z.string().min(5).max(50),
-})
+  tags: z.array(z.string().min(1).max(15)).min(1).max(2),
+});
