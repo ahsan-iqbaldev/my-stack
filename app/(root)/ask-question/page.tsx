f06e06b0372bf75758663b0facdb66d6133cacda
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-18 01:43:30 +0500

    Update page.tsx

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,25 +1,23 @@
-import Question from '@/components/shared/forms/Question'
-// import { getUserById } from '@/lib/actions/user.action';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import React from 'react'
+"use client";
+import Question from "@/components/shared/forms/Question";
+import { redirect } from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Page = async () => {
-  const { userId } = auth();
+const Page = () => {
+  const { user } = useSelector((state: any) => state.authentication);
 
-  if (!userId) redirect('/sign-in');
-
-//   const mongoUser = await getUserById({ userId });
+  if (!user) redirect("/sign-in");
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
       <div className="mt-9">
-        <Question mongoUserId={"9823091809380912839812"}/>
+        <Question mongoUserId={"9823091809380912839812"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
