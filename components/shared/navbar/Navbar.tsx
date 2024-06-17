19fcc8b0e78bccfea20a63abe934d2ae59fe8b6f
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-18 01:53:13 +0500

    Update Navbar.tsx

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Theme from './Theme'
-import MobileNav from './MobileNav'
-import GlobalSearch from '../search/GlobalSearch'
+// import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -17,32 +17,31 @@ const Navbar = () => {
           alt="DevFlow"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">My <span className="text-primary-500">Stack</span></p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          My <span className="text-primary-500">Stack</span>
+        </p>
       </Link>
 
       <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
-
-        <SignedIn>
-          <UserButton 
-            afterSignOutUrl="/"
-            appearance={{
-              elements: {
-                avatarBox: 'h-10 w-10'
-              },
-              variables: {
-                colorPrimary: '#ff7000'
-              }
-            }}
-          />
-        </SignedIn>
-
+        {/* <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-10 w-10",
+            },
+            variables: {
+              colorPrimary: "#ff7000",
+            },
+          }}
+        /> */}
+        {/* <SignedIn></SignedIn> */}
         <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
