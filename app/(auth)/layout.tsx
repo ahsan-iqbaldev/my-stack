f8df455af618e4044a43604f8c92737bcf4db085
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-18 01:39:27 +0500

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+"use client";
+import { redirect } from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useSelector((state: any) => state.authentication);
+  if (user) redirect("/");
   return (
-    <main className="flex min-h-screen w-full items-center justify-center">
+    <main className="flex min-h-screen w-full items-center justify-center bg-auth-dark bg-cover bg-center">
       {children}
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
