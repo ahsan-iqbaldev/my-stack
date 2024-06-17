66306b9b1b4ed3f1d0656828c546e2298046be9f
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-18 01:52:44 +0500

    Update LeftSidebar.tsx

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,32 +1,46 @@
-"use client"
+"use client";
 
-import { sidebarLinks } from '@/constants';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Button } from '../ui/button';
-import { SignedOut, useAuth } from '@clerk/nextjs';
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect, usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch, ThunkDispatch } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { sigOutUser } from "@/store/slices/authSlice";
+// import { SignedOut, useAuth } from '@clerk/nextjs';
 
 const LeftSidebar = () => {
-  const { userId } = useAuth();
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+  const { user } = useSelector((state: any) => state.authentication);
+  const userId = user?.userId;
   const pathname = usePathname();
-  
+
+  const handleLogout = () => {
+    dispatch(
+      sigOutUser({ onSuccess: () => toast.success("Logged out successfully") })
+    );
+  };
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
-          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
 
-          if(item.route === '/profile') {
-            if(userId) {
-              item.route = `${item.route}/${userId}`
+          if (item.route === "/profile") {
+            if (userId) {
+              item.route = `${item.route}/${userId}`;
             } else {
               return null;
             }
           }
 
           return (
-              <Link
+            <Link
               href={item.route}
               key={item.label}
               className={`${
@@ -35,51 +49,77 @@ const LeftSidebar = () => {
                   : "text-dark300_light900"
               }  flex items-center justify-start gap-4 bg-transparent p-4`}
             >
-                <Image 
-                  src={item.imgURL}
-                  alt={item.label}
-                  width={20}
-                  height={20}
-                  className={`${isActive ? "" : "invert-colors"}`}
-                />
-                <p className={`${isActive ? 'base-bold' : 'base-medium'} max-lg:hidden`}>{item.label}</p>
-              </Link>
-          )
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
         })}
       </div>
 
-      <SignedOut>
-          <div className="flex flex-col gap-3">
-              <Link href="/sign-in">
-                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                  <Image 
-                    src="/assets/icons/account.svg"
-                    alt="login"
-                    width={20}
-                    height={20}
-                    className="invert-colors lg:hidden"
-                  /> 
-                  <span className="primary-text-gradient max-lg:hidden">Log In</span>
-                </Button>
-              </Link>
+      {!user ? (
+        <div className="flex flex-col gap-3 mt-2">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/account.svg"
+                alt="login"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
+            </Button>
+          </Link>
 
-          
-              <Link href="/sign-up">
-                <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
-                <Image 
-                    src="/assets/icons/sign-up.svg"
-                    alt="sign up"
-                    width={20}
-                    height={20}
-                    className="invert-colors lg:hidden"
-                  /> 
-                  <span className="max-lg:hidden">Sign up</span>
-                </Button>
-              </Link>
-          </div>
-        </SignedOut>
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                alt="sign up"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign up</span>
+            </Button>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3 mt-2">
+          <Button
+            className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+            onClick={() => handleLogout()}
+          >
+            <Image
+              src="/assets/icons/account.svg"
+              alt="login"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="primary-text-gradient max-lg:hidden">Log Out</span>
+          </Button>
+        </div>
+      )}
+
+      {/* <SignedOut>
+        </SignedOut> */}
     </section>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
