bd82a3980d996436031b30c3063c64c1ae0d80a4
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-25 01:19:49 +0500

    Update page.tsx

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 import Profile from "@/components/shared/forms/Profile";
+import { getProfile } from "@/store/slices/profileSlice";
 // import { getUserById } from '@/lib/actions/user.action';
 import { ParamsProps } from "@/types";
+import { ThunkDispatch } from "@reduxjs/toolkit";
 // import { auth } from '@clerk/nextjs'
 import { redirect } from "next/navigation";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Page = ({ params }: ParamsProps) => {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
   const { user } = useSelector((state: any) => state.authentication);
+  const { profile } = useSelector((state: any) => state.profile);
   const userId = user?.userId;
 
   if (!user) redirect("/sign-in");
+
+  useEffect(() => {
+    dispatch(
+      getProfile({
+        userId,
+      })
+    );
+  }, []);
 
   //   const mongoUser = await getUserById({ userId })
 
@@ -19,7 +32,7 @@ const Page = ({ params }: ParamsProps) => {
       <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
 
       <div className="mt-9">
-        <Profile clerkId={userId} user={user} />
+        <Profile clerkId={userId} profile={profile} />
       </div>
     </>
   );
